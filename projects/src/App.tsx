3f44c7f902f2ab22b3f4ac645eb17e182122a243
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-07-05 22:51:18 -0600

    Update project list

--- projects/src/App.tsx
+++ projects/src/App.tsx
@@ -21,13 +21,6 @@ const projects: Data[] = [
     sourceCodeUrl: "https://github.com/ljven00/OdinProject/tree/main/etch-a-sketch"
   },
   {
-    imgUrl: "https://cdn.pixabay.com/photo/2015/06/24/15/45/code-820275_1280.jpg",
-    title: "Fruits Count",
-    preview: true,
-    previewUrl: "",
-    sourceCodeUrl: ""
-  },
-  {
     imgUrl: passGen,
     title: "Password Generator",
     preview: true,
